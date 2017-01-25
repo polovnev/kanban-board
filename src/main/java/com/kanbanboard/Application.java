2613fe5015bc730ddc4c1e780e49dd6b7674cb1f
polovnevad@gmail.com <polovnevad@gmail.com>
2017-01-25 13:47:22 +0200

    finish

--- src/main/java/com/kanbanboard/Application.java
+++ src/main/java/com/kanbanboard/Application.java
@@ -3,12 +3,14 @@ package com.kanbanboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 
 
 @SpringBootApplication()
 public class Application {
 
     public static void main(String[] args) throws Exception {
+
         SpringApplication.run(Application.class, args);
         System.out.println("Open http://localhost:8080/");
     }
